b0dd3b380f21527425be6b094fe086dca1501431
Fabrizio Nunnari <fabrizio.nunnari@dfki.de>
2018-03-08 14:37:34 +0100

    Updated attributes exporter to work on any active object

--- CharacterGeneration/BlenderScenes/ExportMBLabAttributesInfo.py
+++ CharacterGeneration/BlenderScenes/ExportMBLabAttributesInfo.py
@@ -13,48 +13,61 @@ import csv
 import os
 
 
+def export_mblab_attributes(mesh_obj:bpy.types.Object, outfilepath: str) -> None:
+
+    #
+    # List attributes
+    print("Start gathering attributes...")
+
+    attributes = []
+
+    # Filter for float only attributes
+    for attr in inspect.getmembers(mesh_obj, lambda a: isinstance(a, float)):
+        attribute_name = attr[0]  # type: str
+        # filter for uppercase only attributes
+        if attribute_name[0].isupper():
+            # print(attr[0])
+            if not attribute_name.startswith("Expressions_"):
+                attributes.append({'name': attr[0], 'type': 'nc'})  # 'nc' stands for 'numerical continuous'
+
+    print("Found {} attributes.".format(len(attributes)))
+
+    print("Writing csv file '{}'...".format(outfilepath))
+
+    #
+    # Write attribute list to a file
+
+    # Useful paths
+    scene_path = bpy.data.filepath
+    scene_dir, scene_file = os.path.split(scene_path)
+    attributes_out_filename = os.path.join(scene_dir, outfilepath)
+
+    print("Writing attributes to file '{}'...".format(attributes_out_filename))
+
+    with open(attributes_out_filename, 'w') as csvfile:
+        fieldnames = ['name', 'type']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for attribute in attributes:
+            writer.writerow(attribute)
+
+    print("Finished.")
+
+
 #
-# Get reference to character mesh
-print("Taking reference to MBLab Mesh...")
-
-MHHuman = bpy.data.objects['f_ca01']
-assert MHHuman is not None
-assert MHHuman.type == 'MESH'
-
+# MAIN
 #
-# List attributes
-print("Start gathering attributes...")
 
-attributes = []
+if __name__ == "__main__":
+    scene = bpy.context.scene
 
-# Filter for float only attributes
-for attr in inspect.getmembers(MHHuman, lambda a: isinstance(a, float)):
-    # filter for uppercase only attributes
-    if attr[0][0].isupper():
-        # print(attr[0])
-        attributes.append({'name': attr[0], 'type': 'nc'})  # 'nc' stands for 'numerical continuous'
+    mesh_obj = bpy.context.active_object  # type: bpy.types.Object
+    assert isinstance(mesh_obj, bpy.types.Object)
+    assert mesh_obj.type == 'MESH'
 
-print("Finished. Found {}!".format(len(attributes)))
-print("Writing csv file...", end='')     
-        
+    arm_obj = mesh_obj.parent  # bpy.types.Object
+    assert isinstance(arm_obj, bpy.types.Object)
+    assert arm_obj.type == 'ARMATURE'
 
-#
-# Write attribute list to a file
-
-# Useful paths
-scene_path = bpy.data.filepath
-scene_dir, scene_file = os.path.split(scene_path)
-attributes_out_filename = os.path.join(scene_dir, 'mbast_attributes.csv')
-
-print("Writing attributes to file '{}'...".format(attributes_out_filename))
-
-with open(attributes_out_filename, 'w') as csvfile:
-    fieldnames = ['name', 'type']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    for attribute in attributes:
-        writer.writerow(attribute)
-        
-        
-print("Finished. File written to {} !".format(attributes_out_filename))
+    export_mblab_attributes(mesh_obj=mesh_obj, outfilepath="mblab_attributes.csv")
