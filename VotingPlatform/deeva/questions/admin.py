7088b52399da84513803f7d2e87a7c6acacd58ba
Nicolas Erbach <s9nierba@stud.uni-saarland.de>
2018-08-09 16:06:36 +0200

    completely overhauled questions admin for all models, changed unicode to str.  fixes #30 #31

--- VotingPlatform/deeva/questions/admin.py
+++ VotingPlatform/deeva/questions/admin.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
 
-from .models import Question, Answer, QuestionSet
+from .models import Question, Answer, QuestionSet, SetHasQuestion
 
 # Register your models here.
 
-admin.site.register(Question)
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    #list
+    list_display = ('internal_name', 'title', 'qtype',)
+    list_display_links = ('internal_name',)
 
-admin.site.register(Answer)
 
-admin.site.register(QuestionSet)
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    #list
+    list_display = ('user', 'question', 'answer', 'answered_on')
+    list_display_links = ('user', 'question', 'answer',)
+
+
+class SetHasQuestionInline(admin.TabularInline):
+    model = SetHasQuestion
+    extra = 0
+
+@admin.register(QuestionSet)
+class QuestionSetAdmin(admin.ModelAdmin):
+    #list
+    list_display = ('name', 'questions_list',)
+    list_display_links = ('name',)
+
+    #page
+    inlines = (SetHasQuestionInline,)
--- VotingPlatform/deeva/questions/models.py
+++ VotingPlatform/deeva/questions/models.py
@@ -10,6 +10,7 @@ from django.db import models
 class Question(models.Model):
     Question_Choices = [('B', 'boolean'), ('L', 'list'), ('D', 'date'), ('T', 'text'), ('C', 'country')]
     
+    internal_name = models.CharField(unique=True, max_length=64, help_text="A short internal name to easily select questions.") 
     title = models.TextField(help_text="The question to be asked.")
     help_text = models.TextField(null=True, blank=True, help_text="An optional help text, to clear up what is expected from the user.")
     qtype = models.CharField(max_length=1, choices=Question_Choices, help_text="Boolean: will be rendered as 'Yes/No'. List displays a drop-down menu. Text is a free-text answer.") 
@@ -34,8 +35,8 @@ class Question(models.Model):
             if not len(self.answer_possibilities_list()) == len(set(self.answer_possibilities_list())):
                 raise ValidationError("Having duplicates in answer possibilities is not allowed.")
 
-    def __unicode__(self):
-        return self.title
+    def __str__(self):
+        return self.internal_name
 
 
 #An answer for a question by a user
@@ -46,8 +47,8 @@ class Answer(models.Model):
     answer = models.TextField()
     answered_on = models.DateTimeField(auto_now=True)
 
-    def __unicode__(self):
-        return self.answer
+    def __str__(self):
+        return "{} + {} = {}".format(self.user, self.question, self.answer)
 
 
 
@@ -56,10 +57,13 @@ class Answer(models.Model):
 
 #Group several questions in a set and set if they are required to be answered by the user
 class QuestionSet(models.Model):
-    name = models.CharField(max_length = 256)
+    name = models.CharField(unique=True, max_length = 255)
     questions = models.ManyToManyField('Question', through='SetHasQuestion')
 
-    def __unicode__(self):
+    def questions_list(self):
+        return list(self.questions.all().values_list('internal_name', flat=True))
+
+    def __str__(self):
         return self.name
 
 #Connection model between Set and Questions
