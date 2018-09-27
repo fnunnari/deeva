1b06e054bcbc62d6cb654a7456a6d19a701a3be7
Fabrizio Nunnari <fab.nunnari@gmail.com>
2018-09-27 13:18:30 +0200

    Updated the home page + minor format cleanups.

--- VotingPlatform/deeva/experiments/urls.py
+++ VotingPlatform/deeva/experiments/urls.py
@@ -14,16 +14,16 @@ urlpatterns = [
     url(r'^wiz/(?P<wizard_id>[0-9]+)/rate_vote$', views.rate_vote, name='rate_vote'),
 
 
-	url(r'wiz/(?P<wizard_id>\w+)/end', views.wizard_exit, name='wizard_exit'),
-	url(r'wiz/(?P<wizard_id>\w+)/infos', views.wizard_personalinfos, name='wizard_personalinfos'),
-	#url(r'wiz/(?P<wizard_id>\w+)/vote', views.wizard_vote, name='wizard_vote'),
-	url(r'wiz/(?P<wizard_id>\w+)/example', views.wizard_example, name='wizard_example'),
-	url(r'wiz/(?P<wizard_id>\w+)/disclaimer', views.wizard_disclaimer,
-	name='wizard_disclaimer'),
-	url(r'wiz/(?P<wizard_id>\w+)/welcome', views.wizard_welcome, name='wizard_welcome'),
-	url(r'wiz/(?P<wizard_id>\w+)/checkuser', views.wizard_checkuser, name='wizard_checkuser'),
-	url(r'wiz/(?P<wizard_id>\w+)', views.wizard_start, name='wizard_start'),
-    
+    url(r'wiz/(?P<wizard_id>\w+)/end', views.wizard_exit, name='wizard_exit'),
+    url(r'wiz/(?P<wizard_id>\w+)/infos', views.wizard_personalinfos, name='wizard_personalinfos'),
+    # url(r'wiz/(?P<wizard_id>\w+)/vote', views.wizard_vote, name='wizard_vote'),
+    url(r'wiz/(?P<wizard_id>\w+)/example', views.wizard_example, name='wizard_example'),
+    url(r'wiz/(?P<wizard_id>\w+)/disclaimer', views.wizard_disclaimer,
+        name='wizard_disclaimer'),
+    url(r'wiz/(?P<wizard_id>\w+)/welcome', views.wizard_welcome, name='wizard_welcome'),
+    url(r'wiz/(?P<wizard_id>\w+)/checkuser', views.wizard_checkuser, name='wizard_checkuser'),
+    url(r'wiz/(?P<wizard_id>\w+)', views.wizard_start, name='wizard_start'),
+
     url(r'^individuals/content/(?P<individual_id>[0-9]+)/(?P<content_name>[\w.]+)$', views.send_individual_content, name='send_individual_content'),
 
     url(r'^$', views.index, name='index'),
