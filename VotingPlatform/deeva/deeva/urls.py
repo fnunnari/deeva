bdd56fdcf2dd387cb73588039e8b3a191ec243cd
Fabrizio Nunnari <fab.nunnari@gmail.com>
2018-09-27 13:18:00 +0200

    Added the impressed to the base page.

--- VotingPlatform/deeva/deeva/urls.py
+++ VotingPlatform/deeva/deeva/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url, include
 from django.contrib import admin
+
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include('experiments.urls_admin', namespace="experiments_admin")),
     url(r'^admin/', admin.site.urls),
+    url(r'^impressum/', TemplateView.as_view(template_name="impressum.html"), name="impressum"),
     url(r'^news/', include('news.urls')),
     url(r'^', include('experiments.urls', namespace="experiments")),
 ]
